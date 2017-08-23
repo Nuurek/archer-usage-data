e529fd60894c62a29a0a9acbb4be8f1ed0a67d67
Nuurek <nurskijakub@gmail.com>
2017-08-23 14:19:17 +0100

    First version with nouislider.

--- src/app/jobs/chart/chart.component.ts
+++ src/app/jobs/chart/chart.component.ts
@@ -1,7 +1,9 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { JobsService } from '../jobs.service';
 import { Component, OnInit } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
 import { NouisliderComponent } from 'ng2-nouislider';
+
 
 @Component({
   selector: 'app-chart',
@@ -11,27 +13,34 @@ import { NouisliderComponent } from 'ng2-nouislider';
 export class ChartComponent implements OnInit {
   jobs: Object[] = [];
 
-  fraction = 0.05;
+  public fraction = 0.05;
   frameWidth = 0.2;
   showXAxisLabel = true;
   showYAxisLabel = true;
   xAxisLabel = 'Memory usage [MB]';
   yAxisLabel = 'Nodes [pc.]';
+  public disabled = false;
+
+  public someRange: number[] = [3, 6];
 
   public constructor(
     private jobsService: JobsService,
-    private ref: ChangeDetectorRef
   ) {
     setInterval(() => {
-      this.fraction = (this.fraction + this.frameWidth / 2) % 1;
-      this.updateJobs();
+      // console.log(this.someRange);
+      // this.updateJobs();
     }, 2000);
   }
 
   ngOnInit() {
     this.updateJobs();
   }
-  
+
+  onChange(value: any) {
+    console.log('Value changed to', value);
+    this.someRange = value;
+  }
+
   private updateJobs() {
     this.jobsService.getJobsByProject(this.fraction, this.frameWidth).then(jobs => this.jobs = jobs);
   }
